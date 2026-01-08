213ab9a9c753ccc7c1821917f445b85e5e9b9e0b
cheesecake <leyna.fonda@gmail.com>
2026-01-08 20:15:16 +0100

    help j'arrive pas a faire un foutu death screen💔

---
+++ wonderhoy/wonderhoy/die.gd
@@ -0,0 +1,15 @@
+extends Node
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+
+	pass
+
+
+#help j'arrive passs :(
+# en gros je veux si colisionshape 2D est sur un autre colistionshape 2D bah death screen :~] 
