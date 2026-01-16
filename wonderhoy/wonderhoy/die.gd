475f1ca35d7a6a34150f2ffd9e91ba387de8236d
cheesecake <leyna.fonda@gmail.com>
2026-01-16 21:34:15 +0100

    🌠

--- wonderhoy/wonderhoy/die.gd
+++ wonderhoy/wonderhoy/die.gd
@@ -13,3 +13,4 @@ func _process(delta: float) -> void:
 
 #help j'arrive passs :(
 # en gros je veux si colisionshape 2D est sur un autre colistionshape 2D bah death screen :~] 
+#az-(zqugsdcf_vec"lnç_y çcryi" ç ynrec< o"ecr èlno_"ryiorui"eynuio"crxyn"ynoox"eéx'ow-n_çoxe"tè_zxewtnè_oç"wroéextwn"xçx'rçnçotè x"wx"rç"trxntbè_norx""rè_otbxb_oxo"ft xn_nè_txftnè_àx"nèft è_oxf"nè_ot x"ftox"r_otèx"trxrtè_ç uw "x"è_
