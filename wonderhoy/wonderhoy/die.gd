e61c7b0861e9aae6a648da10c7b69bdd32b55236
cheesecake <leyna.fonda@gmail.com>
2026-01-29 20:19:26 +0100

    ragebait

--- wonderhoy/wonderhoy/die.gd
+++ wonderhoy/wonderhoy/die.gd
@@ -14,4 +14,4 @@ func _process(delta: float) -> void:
 
 #help j'arrive passs :(
 # en gros je veux si colisionshape 2D est sur un autre colistionshape 2D bah death screen :~] 
-#why scripting so hard vruh 💔
+#why scripting so hard vruh 💔 the script wants me to say "so the gazelle thinks he can challenge the lion. how adorable. outwardly you pose as a mighty fire breathing dragon, but on the inside you are nothing but a scared, grovenling, snot nosed little rodent. if at once you do not CEASE with your insolence, your impertinence, your insubordination. i shall personally see to it that you are eviscerated form this mortal plane. "
