3869c8554dda7b3889c2ea0425b88d2d372ee865
cheesecake <leyna.fonda@gmail.com>
2026-02-26 20:07:53 +0100

    j'ai fait de mon mieux

--- wonderhoy/wonderhoy/scripts/die.gd
+++ wonderhoy/wonderhoy/scripts/die.gd
@@ -7,11 +7,9 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	when dead = true get value -01 
-	if dead value = -01 restart game #:)
-	pass
+	if dead == 0 :
+		get_tree().reload_current_scene()
 
 
 #help j'arrive passs :(
-# en gros je veux si colisionshape 2D est sur un autre colistionshape 2D bah death screen :~] 
 #why scripting so hard vruh 💔 the script wants me to say "so the gazelle thinks he can challenge the lion. how adorable. outwardly you pose as a mighty fire breathing dragon, but on the inside you are nothing but a scared, grovenling, snot nosed little rodent. if at once you do not CEASE with your insolence, your impertinence, your insubordination. i shall personally see to it that you are eviscerated form this mortal plane. "
