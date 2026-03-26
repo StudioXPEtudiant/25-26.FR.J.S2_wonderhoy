96d65bf1634712f37bdf3693840e96941dbd320a
cheesecake <leyna.fonda@gmail.com>
2026-03-26 19:33:42 +0100

    hmmm j'ai même pas ouver godot pk sa a changer

--- wonderhoy/wonderhoy/scripts/die.gd
+++ wonderhoy/wonderhoy/scripts/die.gd
@@ -11,5 +11,5 @@ func _process(delta: float) -> void:
 		get_tree().reload_current_scene()
 
 
-#help j'arrive passs :(
+#help j'arrive passs :
 #why scripting so hard vruh 💔 the script wants me to say "so the gazelle thinks he can challenge the lion. how adorable. outwardly you pose as a mighty fire breathing dragon, but on the inside you are nothing but a scared, grovenling, snot nosed little rodent. if at once you do not CEASE with your insolence, your impertinence, your insubordination. i shall personally see to it that you are eviscerated form this mortal plane. "
